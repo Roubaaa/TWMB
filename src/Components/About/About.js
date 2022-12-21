01b6566ebda3b35ebe677b3a2fb714d2d605cd7a
MaryyamSalah <maryyamsalah20@gmail.com>
2022-12-21 22:17:49 +0300

    about section

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,5 +5,21 @@ import "./About.css";
 
 export default function About() {
   return (
-<p>About </p>
-       )}
+    <div>
+<h1 className="absolute top-28 left-52 font-sans font-semibold text-xl">HEALING!</h1>
+
+<p className="absolute top-36 left-52 font-sans  text-s">some cool one liner!</p>
+<p className="absolute top-48 left-52 right-96 font-sans  text-xs break-keep">At Healing, we believe there 
+is a better way to do things. A more valuable way where customers are earned rather than
+bought. We re obsessively passionate about it, and our mission is to help people achieve it. 
+We focus on search engine optimization. It s one of the least understood and least transparent
+ aspects of great marketing, and we see that as an opportunity. We re excited to simplify SEO for 
+ everyone through our software, education, and community.</p>
+
+ <h2 className="absolute bottom-20 top-96 font-sans  text-xl">Our Founding</h2>
+<p className="top-96 bottom-20 bg-cyan-50 absolute font-sans  text-xs break-keep">Healing was founded by Payam Abubakr in 2021. It was called Healing Online, and started as a blog and an online community where some of the world s therapists shared their research and ideas. We launched the Beginner s Guide to Therapy and our first study, and that hub of industry expertise transformed into a small consulting firm and led us to create the Online Therapist of today!</p>
+
+</div>
+
+
+)}
