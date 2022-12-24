12ace84145c71d68bde97f49609773241db89d9c
rokayaamjaad <Rukayaamged733@gmail.com>
2022-12-24 21:11:47 +0300

    home page first half

---
+++ src/Components/Home/button.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Button({p}) {
+  return (
+<button type="button" name="button" className="bg-cyan-400 hover:bg-cyan-600 px-auto py-2 center p-5 rounded-lg text-xl -ml-5 mt-16 ">{p}</button>
+       )}
