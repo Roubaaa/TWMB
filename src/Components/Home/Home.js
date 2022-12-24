c447bbbdfa502c4a679b533488656c1b75564723
shad mustafa <shadmanmi2018@gmail.com>
2022-12-24 21:38:49 +0300

    tickts

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Home.css";
-
+import PurchaseATicket from "../PurchaseATicket/PurchaseATicket"
 
 // import { Link } from "react-router-dom";
 
 
 export default function Home() {
   return (
-   
+   <div>
 <p>Home </p>
-
-
+  <PurchaseATicket/>
+</div>
        )}
