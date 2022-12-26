377f2a5d8bf335eef459b0306b82b73f91c50691
Ahmad Hassoun <ahmadhss366@gmail.com>
2022-12-27 01:32:37 +0200

    Help Div in Home Page Completed

---
+++ src/Components/Help/Help.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Help.css";
+import image from "./image5.png";
+import image2 from "./image6.png";
+
+function Help() {
+  return (
+    <div className='flex flex-row justify-center  gap-44' style={{ backgroundImage:`url(${image})`, backgroundRepeat: 'no-repeat', height:'745px',backgroundSize:'100%',width:'1440px%'}}>
+            <div className='mt-28	'>
+                <h1 className='text-4xl font-normal	'>WE ARE HERE TO</h1>
+                <h1 className='text-9xl	font-normal	'>HELP</h1>
+                <button type="button" className='w-80 h-16 rounded-md font-normal text-2xl mt-20' style={{background: '#2DD3E3'}}>BOOK AN APPOINTMENT</button>
+            </div>
+            <div className='mt-28'>
+                <img src={image2} alt="" className='image2'/>
+            </div>
+    </div>
+  )
+}
+
+export default Help
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./Home.css";
 import PurchaseATicket from "../PurchaseATicket/PurchaseATicket"
-
+import Help from "../Help/Help";
 // import { Link } from "react-router-dom";
 
 
 export default function Home() {
   return (
    <div>
-<p>Home </p>
+  <Help/>
   <PurchaseATicket/>
 </div>
        )}
