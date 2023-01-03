99f36079ab858ab55ff5057c531e2f4a10d42ab7
MaryyamSalah <maryyamsalah20@gmail.com>
2023-01-03 21:26:12 +0300

    Adding Facebook Login with React.js Application

--- src/Components/LogIn/SignIn.js
+++ src/Components/LogIn/SignIn.js
@@ -57,12 +57,13 @@ export default function SignIn() {
               data-mdb-ripple-color="light"
               className="inline-block p-3 bg-cyan-400 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out mx-1"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="w-4 h-4">
+              
+              <Link to="/signInFb"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" className="w-4 h-4">
                 <path
                   fill="currentColor"
                   d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"
                 />
-              </svg>
+              </svg></Link>
             </button> <button
               type="button"
               data-mdb-ripple="true"
