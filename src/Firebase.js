550842be6258ec51e47565c586c50c8f761b9c46
rokayaamjaad <Rukayaamged733@gmail.com>
2023-01-06 20:02:03 +0300

    complete setting firebase

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
+import { getAuth } from "firebase/auth";// TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -18,4 +17,9 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// gives us an auth instance
+const auth = getAuth(app);
+
+// in order to use this auth instance elsewhere
+export default auth;
