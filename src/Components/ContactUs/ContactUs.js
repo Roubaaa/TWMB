1e924f2e9e01f671c21a0078fa6bcef20f96c5c5
rokayaamjaad <Rukayaamged733@gmail.com>
2023-01-14 22:41:13 +0300

    addining questions

--- src/Components/ContactUs/ContactUs.js
+++ src/Components/ContactUs/ContactUs.js
@@ -6,19 +6,81 @@ import {db} from "../../Firebase"
 import image from "./Group.png";
 
 const  ContactUs = ()=> {
-    
+
   const[name , setName] = useState("");
   const[email , setEmail] = useState({});
   const[details , setDetails] = useState("");
   const[questions , setQuestion] = useState("")
+  const[first,setFirst] = useState(true)
+  const[second,setSecond] = useState(true)
+  const[third,setThird] = useState(true)
+  const[fourth,setFourth] = useState(true)
+  const[fifth,setFifth] = useState(true)
+  const[sixth,setSixth] = useState(true)
+  const[seventh,setSeventh] = useState(true)
 
     // __________this handling  email,name,details input___________________
+
+const handleOnChange1 = (data) => {
+
+if (data==='first'){
+     if (first===true) {
+      setQuestion("I have a question about the service.")
+     }
+    setFirst(!first)
+  };
+
+
+  if (data==='second'){
+       if (second===true) {
+        setQuestion("I am a registered client and I need support.")
+       }
+      setSecond(!second)
+    };
+
+if (data==='third'){
+         if (third===true) {
+          setQuestion("I am a counselor interested in joining.")
+         }
+        setThird(!third)
+      };
+
+if (data==='fourth'){
+           if (fourth===true) {
+            setQuestion("I am a registered counselor and I need support.")
+           }
+          setFourth(!fourth)
+        };
+
+if (data==='fifth'){
+             if (fifth===true) {
+              setQuestion("I have a business-related inquiry.")
+             }
+            setFifth(!fifth)
+          };
+if (data==='sixth'){
+               if (sixth===true) {
+                setQuestion("I am interested in Healing Online for my organization.")
+               }
+              setSixth(!sixth)
+            };
+
+if (data==='seventh'){
+                 if (seventh===true) {
+                  setQuestion("I have a billing related question.")
+                 }
+                setSeventh(!seventh)
+              };
+
+};
+
+
   const handleOnChange = (event) => {
     const {
       target: { name: keyName, value },
     } = event;
   // console.log('handleOnChange:', keyName);
-   
+
     setDetails((prev) => {
       return { ...prev, [keyName]: value };
     });
@@ -28,22 +90,20 @@ const  ContactUs = ()=> {
     setName((prev) => {
       return { ...prev, [keyName]: value };
     });
-    setQuestion((prev) => {
-      return { ...prev, [keyName]: value };
-    });
+
   };
 
-  
-  
+
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     // console.log(newEmailInput);
     await addDoc(collection(db, 'contactdata'), {
-      ...email 
+      ...email,questions 
     }).then(() => {
        alert('Message has been submitted');})
     // Clear the form
-   
+
     setName({
       name: '',
     });
@@ -53,11 +113,9 @@ const  ContactUs = ()=> {
     setDetails({
       details: '',
     });
-    setQuestion({
-      questions: '',
-    });
-  };
 
+  };
+console.log(questions)
   // const questionsChangeHandler = (event) => {
   //   // console.log('selected question:', event.target.value);
   //   const {
@@ -79,16 +137,17 @@ const  ContactUs = ()=> {
 <div className="flex flex-row w-10/12	 mt-20 gap-56">
     <div className="w-2/4 mt-6">
       <h1 className="text-2xl font-medium	" >Type of contact</h1>
-      <div className="space-y-5 mt-5" onChange={handleOnChange}>
-       
-        <label htmlFor="ques1">I have a question about the service.<input type="radio" id="ques1" name="questions" value={questions.QuestionA} /></label><br/>
-        <label htmlFor="ques2">I am a registered client and I need support.<input type="radio" id="ques2" name="questions" value={questions.QuestionB} /></label><br/>
-        <label htmlFor="ques3">I am a counselor interested in joining.<input type="radio" id="ques3" name="questions" value={questions.QuestionC}  /></label><br/>
-        <label htmlFor="ques4">I am a registered counselor and I need support.<input type="radio" id="ques4" name="questions" value={questions.QuestionD}  /></label><br/>
-        <label htmlFor="ques5">I have a business-related inquiry.<input type="radio" id="ques5" name="questions" value={questions.QuestionE}  /></label><br/>
-        <label htmlFor="ques6">I am interested in Healing Online for my organization.<input type="radio" id="ques6" name="questions" value={questions.QuestionF}  /></label><br/>
-        <label htmlFor="ques7">I have a billing related question.<input type="radio" id="ques7" name="questions" value={questions.QuestionG}  /></label><br/>
-       
+
+      <div className="space-y-5 mt-5"  >
+
+        <label htmlFor="ques1">I have a question about the service.<input type="radio" id="ques1" name="questions" onChange={()=> handleOnChange1("first")} value={first} /></label><br/>
+        <label htmlFor="ques2">I am a registered client and I need support.<input type="radio" id="ques2" name="questions" onChange={()=> handleOnChange1("second")} value={second} /></label><br/>
+        <label htmlFor="ques3">I am a counselor interested in joining.<input type="radio" id="ques3" name="questions" onChange={()=> handleOnChange1("third")} value={third}  /></label><br/>
+        <label htmlFor="ques4">I am a registered counselor and I need support.<input type="radio" id="ques4" name="questions" onChange={()=> handleOnChange1("fourth")} value={fourth}  /></label><br/>
+        <label htmlFor="ques5">I have a business-related inquiry.<input type="radio" id="ques5" name="questions" onChange={()=> handleOnChange1("fifth")} value={fifth}  /></label><br/>
+        <label htmlFor="ques6">I am interested in Healing Online for my organization.<input type="radio" id="ques6" name="questions" onChange={()=> handleOnChange1("sixth")} value={sixth}  /></label><br/>
+        <label htmlFor="ques7">I have a billing related question.<input type="radio" id="ques7" name="questions" onChange={()=> handleOnChange1("seventh")} value={seventh}  /></label><br/>
+
 
       </div>
     </div>
@@ -99,7 +158,7 @@ const  ContactUs = ()=> {
 
 
 
-</div>    
+</div>
 
 <div className="flex flex-row justify-center items-center my-10">
       <form className="w-10/12" onSubmit={handleSubmit}>
@@ -109,9 +168,9 @@ const  ContactUs = ()=> {
         name="name"
         value={name.name}
         onChange={handleOnChange}
-        
+
         className="rounded-lg shadow-md placeholder:text-lg placeholder border	px-6" style={{width:"604px",height:"68px"}} placeholder="Enter your full name here..."
-      
+
         /></label>
 
         <label htmlFor="email" className="font-normal text-xl">Email:
@@ -126,19 +185,19 @@ const  ContactUs = ()=> {
         />
         </label>
         <label htmlFor="detail" className="font-normal text-xl">Details:
-        <input type="text"  
+        <input type="text"
         name="details"
         value={details.details}
         onChange={handleOnChange}
          className="rounded-lg shadow-md placeholder:text-lg placeholder border px-6" style={{width:"604px",height:"180px"}} placeholder="Enter your details here..."
-         
-      
+
+
         /></label>
         <button  type="submit"
               aria-label="Send"
               className='w-56	 h-16 rounded-md font-normal text-2xl mt-20'
              style={{background: '#2DD3E3'}}>SUBMIT</button>
-        </div> 
+        </div>
       </form>
       <div className="flex w-10/12 justify-center items-center" style={{height:'581.71px'}}>
         <div className="flex flex-col rounded-lg w-96	justify-center items-start text-2xl" style={{background:"#EAF8F9", borderRadius:'35px'}}>
