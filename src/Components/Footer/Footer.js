99f2742f44dc4f2cf37bc60d6dd5a5e168b27a34
Ruba Alsoheil <rubasoheil@gmail.com>
2023-01-19 22:33:11 +0200

    solve

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Footer.css";
 import { Link } from "react-router-dom";
 import { IoPaperPlane } from "react-icons/io5";
 import { AiOutlineTwitter, AiOutlineGoogle} from "react-icons/ai";
 import { SiFacebook } from "react-icons/si";
-
+import { collection, addDoc } from "firebase/firestore";
+import db from "../../Firebase/Firebase";
 
 
 export default function Footer() {
+
+// Subscribe to newsletter
+
+  const [input, setInput] = useState(""); 
+  const inputHandler = (e) => {
+    e.preventDefault();
+    setInput(e.target.value)
+  }; 
+
+
+
+  const submitHandler = async (event) => {
+    event.preventDefault();
+    await addDoc(collection(db, "Newsletter"), {
+      email: input
+    });
+
+    setInput("")
+  };
+  
   return (
 
       
@@ -36,19 +57,24 @@ export default function Footer() {
     </p>
     <div className="searchparent sm:flex sm:items-center sm:justify-between ">
     
-    <div className="search flex items-center justify-between border-gray-500">
-          <input
-            type="text"
+    <form className="search flex items-center justify-between border-gray-500" onSubmit={submitHandler}>
+      
+        <input
+            type="email"
+             onChange ={inputHandler}
+            value ={input}
             placeholder="Enter Your e-mail"
             className=" searchh "
           />
-          <button type="button"
+          <button type="submit"
             className="btn duration-300 px-5 py-2.5 font-[Poppins]
             text-black md:w-auto border-gray-500"
           >
+            
         <IoPaperPlane />
           </button>
-        </div>
+          
+        </form>
           
         <div className="icons flex mt-4 space-x-6 sm:justify-center sm:mt-0">
 
