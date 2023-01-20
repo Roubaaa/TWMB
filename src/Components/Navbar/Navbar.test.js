98341a19b468945453698446a03ea1b69f31819f
Ruba Alsoheil <rubasoheil@gmail.com>
2023-01-20 12:16:49 +0200

    test

---
+++ src/Components/Navbar/Navbar.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Navbar from "./Navbar"
+import { render, screen } from '@testing-library/react';
+
+
+describe("Tests for NavBar Router", () => {
+  test("Website headings", () => {
+    // Arrange
+
+
+    // Act
+    render(
+   
+        <Navbar/>
+
+    );
+
+    // Assert
+
+  }); }
+)
