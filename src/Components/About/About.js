8ac001557ba9bfafe40c6c32c9ca90fe11754274
MaryyamSalah <maryyamsalah20@gmail.com>
2023-01-25 20:28:26 +0300

    adding team's profile

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./About.css";
-import finder from "./finder.jpg"
+import luck from "./luck.jpg"
 // import { Link } from "react-router-dom";
 
 
@@ -10,8 +10,8 @@ export default function About() {
       <div>
 <h1 className="sm:px-48 px-2 pt-8 font-normal font-sans text-xl  md:text-2xl">HEALING!</h1>
 
-<p className="sm:px-48 px-2 text-zinc-500 font-sans text-xs sm:text-sm md:text-s ">some cool one liner!</p>
-<p className="sm:px-48 px-2 pt-8 pb-12 font-sans text-xs sm:text-sm md:text-l  break-keep">At Healing, we believe there 
+<p className="sm:px-48 px-2 text-zinc-500 font-sans text-xs sm:text-sm md:text-s normal-case ">some cool one liner!</p>
+<p className="sm:px-48 px-2 pt-8 pb-12 font-sans text-xs sm:text-sm md:text-l  break-keep normal-case">At Healing, we believe there 
 is a better way to do things. A more valuable way where customers are earned rather than
 bought. We re obsessively passionate about it, and our mission is to help people achieve it. 
 We focus on search engine optimization. It s one of the least understood and least transparent
@@ -19,10 +19,10 @@ We focus on search engine optimization. It s one of the least understood and lea
  everyone through our software, education, and community.</p>
  </div>
 <div className="sm:py-2 bg-cyan-50 flex items-stretch ">
-<img className="sm:w-56 w-56 h-56 sm:ml-56 ml-2 sm:py-2 py-8" src={finder} alt="finder" />
+<img className="sm:w-56 w-56 h-56 sm:ml-56 ml-2 sm:py-2 py-8" src={luck} alt="luck" />
 <div>
 <h2 className="sm:px-16 px-4 pt-4 font-sans  text-xl sm:text-xl md:text-2xl">Our Founding</h2>
-<p className="sm:px-16 px-4 sm:pr-32 font-sans pt-2 pb-8 text-xs sm:text-sm md:text-l ">Healing was founded by Payam Abubakr in 2021. It was called Healing Online, and started as a blog and an online community where some of the world s therapists shared their research and ideas. We launched the Beginner s Guide to Therapy and our first study, and that hub of industry expertise transformed into a small consulting firm and led us to create the Online Therapist of today!</p>
+<p className="sm:px-16 px-4 sm:pr-32 font-sans pt-2 pb-8 text-xs sm:text-sm md:text-l normal-case">Healing was founded  in 2021. It was called Healing Online, and started as a blog and an online community where some of the world s therapists shared their research and ideas. We launched the Beginner s Guide to Therapy and our first study, and that hub of industry expertise transformed into a small consulting firm and led us to create the Online Therapist of today!</p>
  </div>
  </div>
 
--- src/Components/About/Meet.js
+++ src/Components/About/Meet.js
@@ -1,6 +1,10 @@
 import React from "react";
 import "./About.css";
-import finder from "./finder.jpg"
+import maryam from "./maryam.jpg";
+import rukaya from "./rukaya.jpg";
+import ruba from "./ruba.jpg";
+import ahmad from "./ahmad.png";
+import shad from "./shad.jpg";
 // import { Link } from "react-router-dom";
 
 
@@ -10,54 +14,46 @@ export default function Meet() {
       <div>
 <h1 className="sm:px-48 px-2 pt-12 font-normal font-sans text-2xl  md:text-3xl">WE ARE HEALING, NICE TO MEET YOU!</h1>
 
-<p className="sm:px-48 px-2 sm:pt-20 text-zinc-500 font-normal font-sans text-lg  ">Meet the Team!</p>
+<p className="sm:px-48 px-2 sm:pt-20 text-zinc-500 font-normal font-sans text-lg normal-case ">Meet the Team!</p>
  </div>
  <div className="sm:pl-48 px-2 flex flex-wrap sm:flex-row">
 <div className=" sm:pl-0 pr-4 px-2 py-8">
-<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={finder} alt="finder" />
+<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={maryam} alt="maryam" />
 <div className=" w-28 h-36 bg-cyan-50 rounded-b-3xl text-center text-zinc-500 px-2">
-<h2 className="text-s sm:text-sm md:text-s py-4">Allan Saleh</h2>
-<p className="text-xs sm:text-xs md:text-s">Lead Engineer & Web Developer</p>
+<h2 className="text-s sm:text-sm md:text-s py-4">Maryyam Salah</h2>
+<p className="text-xs sm:text-xs md:text-s normal-case">Lead Engineer & Web Developer</p>
 
 </div>
 </div>
-<div className=" sm:px-4 px-2 py-8">
-<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={finder} alt="finder" />
+<div className=" sm:px-8 px-2 py-8">
+<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={rukaya} alt="rukaya" />
 <div className=" w-28 h-36 bg-cyan-50 rounded-b-3xl text-center text-zinc-500 px-2">
-<h2 className="text-s sm:text-sm md:text-s py-4">Allan Saleh</h2>
-<p className="text-xs sm:text-xs md:text-s">Lead Engineer & Web Developer</p>
+<h2 className="text-s sm:text-sm md:text-s py-4">Rokaya Amjaad</h2>
+<p className="text-xs sm:text-xs md:text-s normal-case">Lead Engineer & Web Developer</p>
 
 </div>
 </div>
-<div className=" sm:px-4 px-2 py-8">
-<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={finder} alt="finder" />
+<div className=" sm:px-8 px-2 py-8">
+<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={ruba} alt="ruba" />
 <div className=" w-28 h-36 bg-cyan-50 rounded-b-3xl text-center text-zinc-500 px-2">
-<h2 className="text-s sm:text-sm md:text-s py-4">Allan Saleh</h2>
-<p className="text-xs sm:text-xs md:text-s">Lead Engineer & Web Developer</p>
+<h2 className="text-s sm:text-sm md:text-s py-4">Ruba Alsoheil</h2>
+<p className="text-xs sm:text-xs md:text-s normal-case">Lead Engineer & Web Developer</p>
 
 </div>
 </div>
-<div className=" sm:px-4 px-2 py-8">
-<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={finder} alt="finder" />
+<div className=" sm:px-8 px-4 py-8">
+<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={shad} alt="shad" />
 <div className=" w-28 h-36 bg-cyan-50 rounded-b-3xl text-center text-zinc-500 px-2">
-<h2 className="text-s sm:text-sm md:text-s py-4">Allan Saleh</h2>
-<p className="text-xs sm:text-xs md:text-s">Lead Engineer & Web Developer</p>
+<h2 className="text-s sm:text-sm md:text-s py-4">Shad Mustafa</h2>
+<p className="text-xs sm:text-xs md:text-s normal-case">Lead Engineer & Web Developer</p>
 
 </div>
 </div>
-<div className=" sm:px-4 px-2 py-8">
-<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={finder} alt="finder" />
+<div className=" sm:px-8 px-2 py-8">
+<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={ahmad} alt="ahmad" />
 <div className=" w-28 h-36 bg-cyan-50 rounded-b-3xl text-center text-zinc-500 px-2">
-<h2 className="text-s sm:text-sm md:text-s py-4">Allan Saleh</h2>
-<p className="text-xs sm:text-xs md:text-s">Lead Engineer & Web Developer</p>
-
-</div>
-</div>
-<div className=" sm:px-4 px-2 py-8">
-<img className="sm:w-28 w-28 rounded-t-3xl border-b-green-500 border-solid  border-b-2" src={finder} alt="finder" />
-<div className=" w-28 h-36 bg-cyan-50 rounded-b-3xl text-center text-zinc-500 px-2">
-<h2 className="text-s sm:text-sm md:text-s py-4">Allan Saleh</h2>
-<p className="text-xs sm:text-xs md:text-s">Lead Engineer & Web Developer</p>
+<h2 className="text-s sm:text-sm md:text-s py-4">Ahmad Hasoun</h2>
+<p className="text-xs sm:text-xs md:text-s normal-case">Lead Engineer & Web Developer</p>
 
 </div>
 </div>
